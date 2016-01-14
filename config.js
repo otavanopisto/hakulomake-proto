46a204cb9f38769eb0eef773163058e6fc4d4fb1
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-01-14 14:55:36 +0200

    ** Added config files

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 3000,
+  server_root: '/',
+  session_secret: 'ok1h34iou54h198h90fh1q890123f8j902',
+  database: {
+    host: 'localhost',
+    table: 'sjapplication'
+  }
+  
+};
