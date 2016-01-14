1814ec28e227526d33a5aea0862343ccc80780a8
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-01-14 14:54:47 +0200

    ** Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var config = require('./config');
+var expressValidator = require('express-validator');
+
+mongoose.connect('mongodb://' + config.database.host + '/' + config.database.table);
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cookieParser());
+app.use(expressSession({secret:config.session_secret}));
+
+app.set('port', config.port);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended : true
+}));
+app.use(expressValidator());
+
+require('./routes')(app, config.server_root);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
