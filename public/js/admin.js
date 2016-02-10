41a97a8372faf798370f2246641c6341d9445586
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-02-10 14:37:09 +0200

    ** Added excel import function

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,9 +4,22 @@
   $('.filter').change(function(){
     var queryString = '';
     $('.filter').each(function(){
-      queryString += '?'+$(this).attr('name')+'='+$(this).val();
+      queryString += $(this).attr('name')+'='+$(this).val()+'&';
     });
-    window.location.href = '/admin'+queryString;
+    window.location.href = '/admin?'+queryString.slice(0, -1);
+  });
+  
+  $('#excelImport').click(function(){
+    var table = $('#applicationTable').DataTable();
+    var data = table
+      .rows()
+      .data();
+    var csvContent = "data:text/csv;charset=utf-8,";
+    for(var i = 0; i < data.length;i++){
+      csvContent += i - 1 < data.length ? data[i].join(';')+ "\n" : data[i].join(',');
+    }
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
   });
   
 	$('#menu-toggle').click(function(e) {
