4d3ef7c593f836637b6f7f0b3241330c6b3e5824
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-02-25 10:43:27 +0200

    ** Removed positions from config

--- config.js
+++ config.js
@@ -10,42 +10,5 @@ module.exports = {
   database: {
     host: 'localhost',
     table: 'sjapplication'
-  },
-  positions: [
-    {department: 'Konsernipalvelut', jobs: [
-      {name: 'Asiointipiste', code: 'asiointipiste'},
-      {name: 'Ruoka- ja puhtauspalvelut', code: 'ruoka'},
-      {name: 'Ympäristöpalvelut', code: 'ymparistopalvelut'},
-      {name: 'Terveysvalvonta', code: 'terveysvalvonta'},
-      {name: 'Tukitehtävät (raivaus- ym.)', code: 'tuki'},
-      {name: 'Opastustehtävät', code: 'opastustehtavat'}
-    ]},
-    {department: 'Sivistystoimi', jobs: [
-      {name: 'Nuoriso', code: 'nuoriso'},
-      {name: 'Liikunta, ulkoilualueet, kentät, jäähalli', code: 'liikuntaulkoilu'},
-      {name: 'Liikunta, uimakoulut ja urheilukoulut', code: 'liikuntakoulut'},
-      {name: 'Kirjasto', code: 'kirjasto'},
-      {name: 'Varhaiskasvatus / päivähoito', code: 'varhaiskasvatus'},
-      {name: 'Kulttuuri', code: 'kulttuuri'}
-    ]},
-    {department: 'Sosiaali- ja terveystoimi', jobs: [
-      {name: 'Piristyspartiot / vanhusten palvelut', code: 'piristys'},
-      {name: 'Akuuttiosastot', code: 'akuuttiosastot'},
-      {name: 'Terveyspalvelut hallinto', code: 'terveyshallinto'},
-      {name: 'Mielenterveys- ja päihdepalvelut', code: 'mielenterveys'}
-    ]},
-    {department: 'Tekninen toimi', jobs: [
-      {name: 'Mittaus- ja kiinteistöt', code: 'mittaus'},
-      {name: 'Asuntomessut / opastehtävät', code: 'asuntomessut'}
-    ]},
-    {department: 'YIT Kuntatekniikka Oy', jobs: [
-      {name: 'Puisto- ja puhtaanapitotyöt', code: 'puisto'},
-    ]},
-    {department: 'Viherpalvelut Hyvönen Oy', jobs: [
-      {name: 'Puisto - ja puhtaanapitotyöt / keskustan alue', code: 'puistokeskusta'},
-    ]},
-    {department: 'Yhdyskuntatekniikka ja ympäristö', jobs: [
-      {name: 'Puisto- ja puhtaanapitotyöt / Ristiina', code: 'puistoristiina'},
-    ]}
-  ]
+  }
 };
