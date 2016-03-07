1cd273efb30b8d3df9604c43e4c81c7a9df3072d
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-03-07 15:00:52 +0200

    ** Changed form handling to support new form

--- routes/components/application.js
+++ routes/components/application.js
@@ -5,6 +5,7 @@ var mailer = require('../../services/mailer');
 var config = require('../../config');
 var async = require('async');
 var _ = require('underscore');
+var moment = require('moment');
 
 exports.getApplication = function(req, res){
   var id = req.params.id;
@@ -54,27 +55,49 @@ exports.updateApplication = function(req, res){
 exports.createApplication = function(req, res){
   req.checkBody('firstnameInput', 'Syötä etunimi').notEmpty();
   req.checkBody('lastnameInput', 'Syötä sukunimi').notEmpty();
+  req.checkBody('birthdayInput', 'Syötä syntymäaika muodossa dd.mm.yyyy').notEmpty().isDate();
+  req.checkBody('ssnInput').notEmpty();
+  req.checkBody('sexInput', 'Syötä sukupuoli').notEmpty().isIn(['male', 'female']);
   req.checkBody('addressInput', 'Syötä osoite').notEmpty();
   req.checkBody('zipcodeInput', 'Syötä postinumero').notEmpty().isInt();
   req.checkBody('cityInput', 'Syötä postitoimipaikka').notEmpty();
+  req.checkBody('countryInput', 'Syötä maa').notEmpty();
+  req.checkBody('municipalityInput', 'Syötä asuinkunta').notEmpty();
+  req.checkBody('nationalityInput', 'Syötä kansalaisuus').notEmpty();
+  req.checkBody('languageInput', 'Syötä äidinkieli').notEmpty();
   req.checkBody('phoneInput', 'Syötä puhelinnumero').notEmpty();
-  req.checkBody('birthdayInput', 'Syötä syntymäaika muodossa dd.mm.yyyy').notEmpty().isDate();
-  req.checkBody('sexInput', 'Syötä sukupuoli').notEmpty().isIn(['male', 'female']);
   req.checkBody('emailInput', 'Syötä sähköpostiosoite').notEmpty().isEmail();
-  req.checkBody('placeofbirthInput', 'Syötä syntymäpaikka').notEmpty();
-  req.checkBody('homeInput', 'Syötä kotipaikka').notEmpty();
-  req.checkBody('educationInpuit', 'Syötä koulutustiedot').notEmpty();
-  req.checkBody('experienceInput', 'Syötä kokemustiedot').notEmpty();
-  req.checkBody('itskillsInput', 'Syötä ATK-taidot').notEmpty();
-  req.checkBody('previouslyEmployedInput', 'Oletko aiemmin työskennellyt kaupungille?').notEmpty().isBoolean();
-  req.checkBody('englishInput', 'Syötä kielitaito').isIn(['good', 'ok', 'bad', '']);
-  req.checkBody('swedishInput', 'Syötä kielitaito').isIn(['good', 'ok', 'bad', '']);
-  req.checkBody('germanInput', 'Syötä kielitaito').isIn(['good', 'ok', 'bad', '']);
-  req.checkBody('suitableWorkingPeriodInput', 'Syötä sopiva työskentely kausi').notEmpty();
-  req.checkBody('additionalInfoInput', 'Syötä lisätiedot').notEmpty();
-  req.checkBody('primaryRequestInput', 'Syötä ensisijainen paikkatoive').notEmpty();
-  req.checkBody('secondaryRequestInput', 'Syötä toissijainen paikkatoive').notEmpty();
-  req.checkBody('othersOk', 'Haluatko työskennellä muissa tehtävissä?').notEmpty().isBoolean();
+  
+  var now = moment().valueOf();
+  var birthday = moment(req.body['birthdayInput']).valueOf();
+  var age = moment.duration(now - birthday).years();
+  
+  if(age < 18){
+    req.checkBody('minorExplanationInput', 'Syötä alaikäisen hakemusperusteet').notEmpty();
+    req.checkBody('parentLastnameInput', 'Syötä huoltajan sukunimi').notEmpty();
+    req.checkBody('parentFirstnameInput', 'Syötä huoltajan etunimi').notEmpty();
+    req.checkBody('parentAddressInput', 'Syötä huoltajan lähiosoite').notEmpty();
+    req.checkBody('parentZipcodeInput', 'Syötä huoltajan postinumero').notEmpty();
+    req.checkBody('parentCityInput', 'Syötä huoltajan postitoimipaikka').notEmpty();
+    req.checkBody('parentCountryInput', 'Syötä huoltajan maa').notEmpty();
+  }
+  
+  req.checkBody('currentlyStudyingInput', 'Opiskeletko parhaillaan?').notEmpty();
+  req.checkBody('currentSchoolInput', 'Syötä nykyinen koulusi').notEmpty();
+  req.checkBody('studyGoalInput', 'Syötä opiskelutavoitteesi').notEmpty();
+  req.checkBody('currentActivityInput', 'Syötä nykyinen tilanteesi').notEmpty();
+  if(req.body.currentActivityInput == '5'){
+    req.checkBody('additionalActivityInput', 'Syötä tarkennus').notEmpty();
+  }
+  req.checkBody('previousStudiesInput', 'Syötä aikaisemmat opinnot').notEmpty();
+  if(req.body.previousStudiesInput == 'Lukio (keskeytynyt)'){
+    req.checkBody('previousCollegeInput', 'Syötä aikaisempi lukio').notEmpty();
+    req.checkBody('previousCollegeDurationInput', 'Syötä aikaisemman lukiokoulutuksen kesto').notEmpty();
+  }else if(req.body.previousStudiesInput == 'Muu'){
+    req.checkBody('additionalPrevStudyInfoInput', 'Syötä tarkennus').notEmpty();
+  }
+  
+  req.checkBody('heardFromInput', 'Mistä kuulit meistä?').notEmpty();
   
   var errors = req.validationErrors();
   if (errors) {
@@ -82,60 +105,49 @@ exports.createApplication = function(req, res){
     res.status(400).send(errors);
   } else {
                       
-    var application = new Application({ 
-      firstName : req.body['firstnameInput'],
+    var application = new Application({
       lastName : req.body['lastnameInput'],
-      address: req.body['addressInput'],
-      zipcode: req.sanitizeBody('zipcodeInput').toInt(),
-      city: req.body['cityInput'],
-      phone: req.body['phoneInput'],
+      firstName : req.body['firstnameInput'],
       birthday: req.body['birthdayInput'],
+      ssn: req.body['ssnInput'],
       sex: req.body['sexInput'],
+      address: req.body['addressInput'],
+      zipcode: req.body['zipcodeInput'],
+      city: req.body['cityInput'],
+      country: req.body['countryInput'],
+      municipality: req.body['municipalityInput'],
+      nationality: req.body['nationalityInput'],
+      language: req.body['languageInput'],
+      phone: req.body['phoneInput'],
       email: req.body['emailInput'],
-      placeOfBirth: req.body['placeofbirthInput'],
-      home: req.body['homeInput'],
-      driversLicence: typeof(req.body['driversLicenceInput']) !== 'undefined' ? req.body['driversLicenceInput'] : [],
-      driversLicenceYear: req.sanitizeBody('driverslicenceyearInput'),
-      education: req.body['educationInpuit'],
-      previousExpericence: req.body['experienceInput'],
-      itSkills: req.body['itskillsInput'],
-      previouslyEmployed: req.sanitizeBody('previouslyEmployedInput').toBoolean(),
-      whenEmployed: req.body['whenEmployedInput'],
-      allergies: typeof(req.body['allergiesInput']) !== 'undefined' ? req.body['allergiesInput'] : [],
-      suitableWorkingPeriod: req.body['suitableWorkingPeriodInput'],
-      transportation: typeof(req.body['transportationInput']) !== 'undefined' ? req.body['transportationInput'] : [],
-      additionalInfo: req.body['additionalInfoInput'],
-      primaryRequest: req.body['primaryRequestInput'],
-      secondaryRequest: req.body['secondaryRequestInput'],
-      othersOk: req.body['othersOk'],
-      basicDirectorCourses: req.body['basicDirectorCoursesInput'],
-      fieldDirectorCourses: req.body['fieldDirectorCoursesInput'],
-      appendices: typeof(req.body['appendices']) !== 'undefined' ? req.body['appendices'] : []
+      parent: {
+        argumentForMinor: req.body['minorExplanationInput'],
+        lastName: req.body['parentLastnameInput'],
+        firstName: req.body['parentFirstnameInput'],
+        address: req.body['parentAddressInput'],
+        zipcode: req.body['parentZipcodeInput'],
+        city: req.body['parentCityInput'],
+        country: req.body['parentCountryInput'],
+      },
+      currentlyStudying: req.sanitizeBody('currentlyStudyingInput').toBoolean(),
+      currentSchool: req.body['currentSchoolInput'],
+      studyGoal: req.body['studyGoalInput'],
+      previousStudies: req.body['previousStudiesInput'],
+      previousStudiesInfo: req.body['additionalPrevStudyInfoInput'],
+      previousCollege: {
+        collegeName: req.body['previousCollegeInput'],
+        collegeDuration: req.body['previousCollegeDurationInput']
+      },
+      heardFrom : req.body['heardFromInput'],
+      heardFromInfo: req.body['heardFromInfoInput'],
+      appendices: req.body['appendices'],
     });
-    
-    if(req.body['englishInput'] !== '') 
-      application.languages.english = req.body['englishInput'];
-    
-    if(req.body['swedishInput'] !== '') 
-      application.languages.swedish = req.body['swedishInput'];
-      
-    if(req.body['germanInput'] !== '') 
-      application.languages.german = req.body['germanInput'];
-    
-    if(typeof(req.body['otherAllergies']) !== 'undefined' && req.body['otherAllergies'] !== ''){
-      if(typeof(application.allergies) !== 'undefined' && application.allergies.length > 0){
-        application.allergies.push(req.body['otherAllergies']);
-      }else{
-        application.allergies = [req.body['otherAllergies']];
-      }
-    }
-    
     
     application.save(function(err, application){
       if(err) {
         console.log(err);
         res.status(400).send(err);
-      }else{
+      }else{ /*
         var content = 'Kesätyöhakemuksesi on vastaanotettu onnistuneesti.'
         try {
           content = jade.renderFile(__dirname+'/../../views/mail.jade', {application: application, positions: config.positions});
@@ -143,6 +155,7 @@ exports.createApplication = function(req, res){
           console.log(renderEx)
         }      
         mailer.sendMail(application.email, 'Kesätyöhakemuksesi on vastaanotettu.', content);
+        */
         res.send(application);
       }
     });
